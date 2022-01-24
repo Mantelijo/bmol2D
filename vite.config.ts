1f7007c398745d6227c77ca33262438550e33c15
Mantas Jonaitis <j.mantelijo@gmail.com>
2022-01-24 15:07:10 +0200

    finalize migration to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import svgr from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	// This changes the out put dir from dist to build
-	// comment this out if that isn't relevant for your project
+	server: {
+		watch: {
+			// https://vitejs.dev/config/#server-watch
+			usePolling: true,
+		},
+	},
 	build: {
 		outDir: "build",
 	},
-	plugins: [reactRefresh(), svgr()],
+	plugins: [react()],
 });
